5e2d493748c99febe0fbb638971f32197e33f228
Yulia <yuliashaifele@gmail.com>
2025-03-01 20:47:49 +0400

    Add resume page from scratch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+$("#themeToggle").click(function () {
+	$("body").toggleClass("dark-mode");
+
+	if ($("body").hasClass("dark-mode")) {
+	  $("#themeToggle").html("☀️ Light Mode");
+	  localStorage.setItem("theme", "dark");
+	} else {
+	  $("#themeToggle").html("🌙 Dark Mode");
+	  localStorage.setItem("theme", "light");
+	}
+  });
+  
+  if (localStorage.getItem("theme") === "dark") {
+	$("body").addClass("dark-mode");
+	$("#themeToggle").html("☀️ Light Mode");
+  }
+  
